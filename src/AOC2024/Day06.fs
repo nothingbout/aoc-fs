90492b6be022a4bfabc70639f6b42693a7e90285
Ville Mäkynen <ville.makynen@gmail.com>
2024-12-07 00:12:40 +0200

    day06 part2 parallelized

--- src/AOC2024/Day06.fs
+++ src/AOC2024/Day06.fs
@@ -47,6 +47,12 @@ let moveOnce map state =
         nextDir <- turnRight nextDir
     State.make (state.Pos + dirToVec nextDir) nextDir (state.Visited |> Set.add (state.Pos, state.Dir))
 
+let getAllStatesUntilOOB map bounds state =
+    state |> Seq.unfold (fun state -> 
+        if State.isOOB bounds state then None
+        else Some (state, moveOnce map state)
+    )
+
 let rec tryFindCycle map traversalGrid state = 
     match TraveralGrid.tryFindInDirection state.Pos (dirToVec state.Dir) traversalGrid with
     | None -> false
@@ -60,31 +66,33 @@ let solveP1 (inputLines: string list) =
     let guardPos, guardDir = inputLines |> parseGuard
     let bounds = Rect.encapsulating map
 
-    State.make guardPos guardDir Set.empty |> Seq.unfold (fun state -> 
-        if State.isOOB bounds state then None
-        else Some (state.Pos, moveOnce map state)
-    ) |> Set.ofSeq |> Set.count |> Answer.int
+    State.make guardPos guardDir Set.empty |> getAllStatesUntilOOB map bounds
+    |> Seq.map (fun state -> state.Pos) |> Set.ofSeq |> Set.count |> Answer.int
 
 let solveP2 (inputLines: string list) = 
     let map = inputLines |> parseMap
     let guardPos, guardDir = inputLines |> parseGuard
     let bounds = Rect.encapsulating map
     let traversalGrid = TraveralGrid.make (Rect.expand (Vec2.make 1 1) bounds) map
-    State.make guardPos guardDir Set.empty |> Seq.unfold (fun state -> 
-        if State.isOOB bounds state then None
-        else 
-        let nextState = moveOnce map state
-        if not (State.hasVisitedPos nextState.Pos state) then 
+
+    let states = State.make guardPos guardDir Set.empty |> getAllStatesUntilOOB map bounds
+
+    let chunks = states |> Array.ofSeq |> Array.indexed |> Array.groupByAndMap (fun (i, _) -> i % System.Environment.ProcessorCount) (fun (_, s) -> s)
+    chunks |> Array.Parallel.map (fun (chunkIdx, chunkStates) -> 
+        // let sw = System.Diagnostics.Stopwatch.StartNew()
+        let insertedObstacles = chunkStates |> Array.choose (fun state -> 
+            let nextState = moveOnce map state
+            if State.hasVisitedPos nextState.Pos state then None // Can't insert an obstacle here since we have already traveled through the position
+            else
             // Insert an obstacle in nextState.Pos and check if it results in a cycle
             let isCycle = state |> tryFindCycle (Set.add nextState.Pos map) (TraveralGrid.addPosition nextState.Pos traversalGrid)
-            if isCycle then Some (Some nextState.Pos, nextState)
-            else Some (None, nextState)
-        else
-            // Can't insert an obstacle here since we have already traveled through the position
-            Some (None, nextState)
-    ) 
-    |> Seq.choose id |> Set.ofSeq 
-    |> Set.remove guardPos |> Set.count |> Answer.int
+            if isCycle then Some nextState.Pos else None
+        )
+        // sw.Stop()
+        // printfn $"task {chunkIdx} completed in {sw.Elapsed.TotalMilliseconds} ms"
+        insertedObstacles
+    ) |> Array.concat
+    |> Set.ofSeq |> Set.remove guardPos |> Set.count |> Answer.int
 
 let getPuzzles () = 
     [
