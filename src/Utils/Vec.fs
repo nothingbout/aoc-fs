1c912357da6822750b22ad1cd73d4aecc345a648
nothingbout <nothingbout@pm.me>
2024-12-06 19:02:47 +0200

    day06 part2 optimized

--- src/Utils/Vec.fs
+++ src/Utils/Vec.fs
@@ -6,7 +6,7 @@ type Vec<'T> = { Arr : 'T array }
     with 
         member this.Item
             with inline get (idx) = this.Arr[idx]
-            and inline set idx value = this.Arr[idx] <- value
+            // and inline set idx value = this.Arr[idx] <- value
         override this.ToString (): string = sprintf "(%s)" (this.Arr |> Seq.map (fun x -> x.ToString()) |> String.concat ", ")
         // static member inline get_Zero () = { X = makeZero (); Y = makeZero () }
         static member inline ( ~- ) (a) = {Arr = Array.map ( ~- ) a.Arr}
@@ -18,16 +18,26 @@ type Vec<'T> = { Arr : 'T array }
         static member inline ( / ) (a : Vec<_>, b : Vec<_>) : Vec<_> = {Arr = Array.map2 ( / ) a.Arr b.Arr}
 
 module Vec = 
-    let inline ofArray arr = {Arr = arr}
-    let inline toArray v = v.Arr
-    let inline item (v : Vec<_>) i = v[i]
-    let inline make2 x y = ofArray [|x; y|]
-    let inline make3 x y z = ofArray [|x; y; z|]
-    let inline make4 x y z w = ofArray [|x; y; z; w|]
-    let inline makeZero n = Array.zeroCreate n |> ofArray
-    let inline map f a = Array.map f (toArray a) |> ofArray
-    let inline map2 f a b = Array.map2 f (toArray a) (toArray b) |> ofArray
-    let inline reduce f a = toArray a |> Array.reduce f
+    let inline wrapArray arr = {Arr = arr}
+    let inline unwrapArray v = v.Arr
+
+    let inline ofArray arr = {Arr = Array.copy arr}
+    let inline toArray v = Array.copy v.Arr
+    let inline ofList list = Array.ofList list |> wrapArray
+    let inline toList v = unwrapArray v |> List.ofArray 
+    let inline ofSeq seq = Array.ofSeq seq |> wrapArray
+    let inline item (v : Vec<_>) idx = v[idx]
+    let inline withItem idx value v = let arr = toArray v in Array.set arr idx value; arr
+    let inline make2 x y = wrapArray [|x; y|]
+    let inline make3 x y z = wrapArray [|x; y; z|]
+    let inline make4 x y z w = wrapArray [|x; y; z; w|]
+    let inline makeZero n = Array.zeroCreate n |> wrapArray
+    let inline init n initializer = Array.init n initializer |> wrapArray
+    let inline map f a = Array.map f (unwrapArray a) |> wrapArray
+    let inline map2 f a b = Array.map2 f (unwrapArray a) (unwrapArray b) |> wrapArray
+    let inline mapi f a = Array.mapi f (unwrapArray a) |> wrapArray
+    let inline mapi2 f a b = Array.mapi2 f (unwrapArray a) (unwrapArray b) |> wrapArray
+    let inline reduce f a = unwrapArray a |> Array.reduce f
     let inline min a b = map2 min a b
     let inline max a b = map2 max a b
     let inline minElem a = reduce min a
@@ -37,3 +47,14 @@ module Vec =
     let inline sum a = reduce (+) a
     let inline product a = reduce ( * ) a
     let inline dot a b = a * b |> sum
+
+    let axisDirs n = 
+        seq { for i = 0 to n - 1 do yield Array.init n (fun j -> if j = i then 1 else 0)
+              for i = 0 to n - 1 do yield Array.init n (fun j -> if j = i then -1 else 0) }
+
+    let rec private diagonalDirLists n = 
+        if n = 0 then Seq.singleton []
+        else seq {  for tail in diagonalDirLists (n - 1) do yield 1 :: tail
+                    for tail in diagonalDirLists (n - 1) do yield -1 :: tail }
+    let diagonalDirs n = diagonalDirLists n |> Seq.map ofList
+
